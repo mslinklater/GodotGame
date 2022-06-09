5eb981a90690545e9cecad3ffce5cd770bc0fbdd
Martin Linklater <mslinklater@gmail.com>
2022-06-09 22:57:56 +0100

    shader

--- Camera.gd
+++ Camera.gd
@@ -5,10 +5,6 @@ var forwardSpeed: float = 0.0
 var sideSpeed: float = 0.0
 var verticalSpeed: float = 0.0
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
 func _process(delta):
 	if Input.is_action_pressed("fly_cam"):
 		if Input.is_action_pressed("move_forward"):
--- SceneManagement/SceneManagementRoot.gd
+++ SceneManagement/SceneManagementRoot.gd
@@ -2,6 +2,10 @@ extends Node
 
 var frontEndScene = load("SceneManagement/SubScenes/front_end.tscn")
 var level1Scene = load("SceneManagement/SubScenes/level_1.tscn")
+var level2Scene = load("SceneManagement/SubScenes/level_2.tscn")
+var level3Scene = load("SceneManagement/SubScenes/level_3.tscn")
+var pauseScene = load("SceneManagement/SubScenes/pause.tscn")
+var levelIsLoaded = false
 
 func _ready():
 	pass # Replace with function body.
@@ -9,18 +13,42 @@ func _ready():
 func _on_Front_End_Button_button_down():
 	print("Front End")
 
-
 func _on_Level_1_Button_button_down():
-	print("Level 1")
-
+	var n = get_tree().get_root().find_node("Level1Root", true, false)
+	if n == null:
+		if levelIsLoaded == false:
+			get_tree().get_root().add_child(level1Scene.instance())
+			levelIsLoaded = true
+	else:
+		n.queue_free()
+		levelIsLoaded = false
 
 func _on_Level_2_Button_button_down():
-	print("Level 2")
+	var n = get_tree().get_root().find_node("Level2Root", true, false)
+	if n == null:
+		if levelIsLoaded == false:
+			get_tree().get_root().add_child(level2Scene.instance())
+			levelIsLoaded = true
+	else:
+		n.queue_free()
+		levelIsLoaded = false
 
 
 func _on_Level_3_Button_button_down():
-	print("Level 3")
-
+	var n = get_tree().get_root().find_node("Level3Root", true, false)
+	if n == null:
+		if levelIsLoaded == false:
+			get_tree().get_root().add_child(level3Scene.instance())
+			levelIsLoaded = true
+	else:
+		n.queue_free()
+		levelIsLoaded = false
 
 func _on_Pause_button_down():
-	print("Pause")
+	var n = get_tree().get_root().find_node("PauseRoot", true, false)
+	if n == null:
+		get_tree().get_root().add_child(pauseScene.instance())
+	else:
+		n.queue_free()
+	
+	
